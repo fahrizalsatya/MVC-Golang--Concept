e90a7fe2336732a9208ed0573e0903f293c91a25
FadhlanHawali <fadhlan.hawali@mail.ugm.ac.id>
2020-09-20 14:32:43 +0700

    add feture add antrian and get antrian

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # Digitalent Kominfo - Pengenalan Konsep MVC Golang
+
+## Development Tutorial
+
+### 1. Install Gin Package
+To see the complete documentation of Gin, you can visit the Gin official [repository][gin-repository].
+```
+go get github.com/gin-gonic/gin
+```
+### 2. Install Firebase Admin SDK 
+```
+go get firebase.google.com/go
+```
+
+[gin-repository]: <https://github.com/gin-gonic/gin>
+
+###2. Initialize Gin Router
+Create the first route path using Gin using this code below.
+```
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main(){
+	router := gin.Default()
+	router.GET("/", getSomething)
+	router.Run(":8080")
+}
+
+func getSomething(c *gin.Context){
+
+	c.JSON(http.StatusOK,map[string]interface{}{
+		"body1":"Get Something Success",
+	})
+
+	return
+}
+```
+To build this, run this command below
+```
+go build 
+```
--- main.go
+++ main.go
@@ -1,5 +1,114 @@
 package main
 
-func main(){
+import (
+	"context"
+	firebase "firebase.google.com/go"
+	"firebase.google.com/go/db"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"google.golang.org/api/option"
+	"log"
+	"net/http"
+)
 
+var client *db.Client
+var ctx context.Context
+
+func init(){
+	ctx = context.Background()
+	conf := &firebase.Config{
+		DatabaseURL: "https://belajarfirebase-e1dd9.firebaseio.com/",
+	}
+	// Fetch the service account key JSON file contents
+	opt := option.WithCredentialsFile("firebase-admin-sdk.json")
+
+	// Initialize the app with a service account, granting admin privileges
+	app, err := firebase.NewApp(ctx, conf, opt)
+	if err != nil {
+		log.Fatalln("Error initializing app:", err)
+	}
+
+	client, err = app.Database(ctx)
+	if err != nil {
+		log.Fatalln("Error initializing database client:", err)
+	}
+}
+
+func main(){
+	router := gin.Default()
+	router.POST("/api/v1/antrian", AddAntrianHandler)
+	router.GET("/api/v1/antrian/status",GetAntrianHandler)
+	router.Run(":8080")
+}
+
+func AddAntrianHandler(c *gin.Context){
+	flag,err := addAntrian()
+	if flag {
+		c.JSON(http.StatusOK,map[string]interface{}{
+			"status":"success",
+		})
+	}else {
+		c.JSON(http.StatusBadRequest,map[string]interface{}{
+			"status":"failed",
+			"error":err,
+		})
+	}
+}
+
+func GetAntrianHandler(c *gin.Context){
+
+	// As an admin, the app has access to read and write all data, regradless of Security Rules
+	//
+	flag,err,resp := getAntrian()
+	if flag {
+		c.JSON(http.StatusOK,map[string]interface{}{
+			"status":"success",
+			"data":resp,
+		})
+	}else {
+		c.JSON(http.StatusBadRequest,map[string]interface{}{
+			"status":"failed",
+			"error":err,
+		})
+	}
+}
+
+func addAntrian() (bool,error){
+	_,_,dataAntrian := getAntrian()
+	var Id string
+	var antrianRef *db.Ref
+	ref := client.NewRef("antrian")
+
+	if dataAntrian == nil{
+		Id = fmt.Sprintf("B-0")
+		antrianRef = ref.Child("0")
+	}else {
+		Id = fmt.Sprintf("B-%d", len(dataAntrian))
+		antrianRef = ref.Child(fmt.Sprintf("%d",len(dataAntrian)))
+	}
+	antrian := Antrian{
+		Id: Id,
+		Status: false,
+	}
+	if err := antrianRef.Set(ctx, antrian); err != nil {
+		log.Fatal(err)
+		return false,err
+	}
+	return true,nil
+}
+
+func getAntrian() (bool,error,[]map[string]interface{}){
+	var data []map[string]interface{}
+	ref := client.NewRef("antrian")
+	if err := ref.Get(ctx, &data); err != nil {
+		log.Fatalln("Error reading from database:", err)
+		return false,err,nil
+	}
+
+	return true,nil,data
+}
+
+type Antrian struct {
+	Id string `json:"id"`
+	Status bool `json:"status"`
 }
