e8bcb52dc4c60a4c4b914189e37823a02901070f
wskurniawan <kurniawan.wisnu23@gmail.com>
2020-09-28 07:39:48 +0700

    add antrian page

--- main.go
+++ main.go
@@ -2,20 +2,21 @@ package main
 
 import (
 	"context"
-	firebase "firebase.google.com/go"
-	"firebase.google.com/go/db"
 	"fmt"
-	"github.com/gin-gonic/gin"
-	"google.golang.org/api/option"
 	"log"
 	"net/http"
 	"strings"
+
+	firebase "firebase.google.com/go"
+	"firebase.google.com/go/db"
+	"github.com/gin-gonic/gin"
+	"google.golang.org/api/option"
 )
 
 var client *db.Client
 var ctx context.Context
 
-func init(){
+func init() {
 	ctx = context.Background()
 	conf := &firebase.Config{
 		DatabaseURL: "https://belajarfirebase-e1dd9.firebaseio.com/",
@@ -35,140 +36,165 @@ func init(){
 	}
 }
 
-func main(){
+func main() {
 	router := gin.Default()
+	router.LoadHTMLGlob("views/*")
+
 	router.POST("/api/v1/antrian", AddAntrianHandler)
-	router.GET("/api/v1/antrian/status",GetAntrianHandler)
-	router.PUT("/api/v1/antrian/id/:idAntrian",UpdateAntrianHandler)
-	router.DELETE("/api/v1/antrian/id/:idAntrian/delete",DeleteAntrianHandler)
+	router.GET("/api/v1/antrian/status", GetAntrianHandler)
+	router.PUT("/api/v1/antrian/id/:idAntrian", UpdateAntrianHandler)
+	router.DELETE("/api/v1/antrian/id/:idAntrian/delete", DeleteAntrianHandler)
+	router.GET("/antrian", PageAntrianHandler)
 	router.Run(":8080")
 }
 
-func AddAntrianHandler(c *gin.Context){
-	flag,err := addAntrian()
+func AddAntrianHandler(c *gin.Context) {
+	flag, err := addAntrian()
 	if flag {
-		c.JSON(http.StatusOK,map[string]interface{}{
-			"status":"success",
+		c.JSON(http.StatusOK, map[string]interface{}{
+			"status": "success",
 		})
-	}else {
-		c.JSON(http.StatusBadRequest,map[string]interface{}{
-			"status":"failed",
-			"error":err,
+	} else {
+		c.JSON(http.StatusBadRequest, map[string]interface{}{
+			"status": "failed",
+			"error":  err,
 		})
 	}
 }
 
-func GetAntrianHandler(c *gin.Context){
-
-	flag,err,resp := getAntrian()
+func GetAntrianHandler(c *gin.Context) {
+	flag, err, resp := getAntrian()
 	if flag {
-		c.JSON(http.StatusOK,map[string]interface{}{
-			"status":"success",
-			"data":resp,
+		c.JSON(http.StatusOK, map[string]interface{}{
+			"status": "success",
+			"data":   resp,
 		})
-	}else {
-		c.JSON(http.StatusBadRequest,map[string]interface{}{
-			"status":"failed",
-			"error":err,
+	} else {
+		c.JSON(http.StatusBadRequest, map[string]interface{}{
+			"status": "failed",
+			"error":  err,
 		})
 	}
 }
 
 func UpdateAntrianHandler(c *gin.Context) {
 	idAntrian := c.Param("idAntrian")
-	flag,err := updateAntrian(idAntrian)
+	flag, err := updateAntrian(idAntrian)
 	if flag {
-		c.JSON(http.StatusOK,map[string]interface{}{
-			"status":"success",
+		c.JSON(http.StatusOK, map[string]interface{}{
+			"status": "success",
 		})
-	}else {
-		c.JSON(http.StatusBadRequest,map[string]interface{}{
-			"status":"failed",
-			"error":err,
+	} else {
+		c.JSON(http.StatusBadRequest, map[string]interface{}{
+			"status": "failed",
+			"error":  err,
 		})
 	}
 }
 
-func DeleteAntrianHandler(c *gin.Context){
+func DeleteAntrianHandler(c *gin.Context) {
 	idAntrian := c.Param("idAntrian")
-	flag,err := deleteAntrian(idAntrian)
+	flag, err := deleteAntrian(idAntrian)
 	if flag {
-		c.JSON(http.StatusOK,map[string]interface{}{
-			"status":"success",
+		c.JSON(http.StatusOK, map[string]interface{}{
+			"status": "success",
 		})
-	}else {
-		c.JSON(http.StatusBadRequest,map[string]interface{}{
-			"status":"failed",
-			"error":err,
+	} else {
+		c.JSON(http.StatusBadRequest, map[string]interface{}{
+			"status": "failed",
+			"error":  err,
 		})
 	}
 }
 
-func addAntrian() (bool,error){
-	_,_,dataAntrian := getAntrian()
+func PageAntrianHandler(c *gin.Context) {
+	flag, err, result := getAntrian()
+	var currentAntrian map[string]interface{}
+
+	for _, item := range result {
+		if item != nil {
+			currentAntrian = item
+			break
+		}
+	}
+
+	if flag && len(result) > 0 {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"antrian": currentAntrian["id"],
+		})
+	} else {
+		c.JSON(http.StatusBadRequest, map[string]interface{}{
+			"status": "failed",
+			"error":  err,
+		})
+	}
+}
+
+func addAntrian() (bool, error) {
+	_, _, dataAntrian := getAntrian()
 	var Id string
 	var antrianRef *db.Ref
 	ref := client.NewRef("antrian")
 
-	if dataAntrian == nil{
+	if dataAntrian == nil {
 		Id = fmt.Sprintf("B-0")
 		antrianRef = ref.Child("0")
-	}else {
+	} else {
 		Id = fmt.Sprintf("B-%d", len(dataAntrian))
-		antrianRef = ref.Child(fmt.Sprintf("%d",len(dataAntrian)))
+		antrianRef = ref.Child(fmt.Sprintf("%d", len(dataAntrian)))
 	}
 	antrian := Antrian{
-		Id: Id,
+		Id:     Id,
 		Status: false,
 	}
 	if err := antrianRef.Set(ctx, antrian); err != nil {
 		log.Fatal(err)
-		return false,err
+		return false, err
 	}
-	return true,nil
+	return true, nil
 }
 
-func getAntrian() (bool,error,[]map[string]interface{}){
+func getAntrian() (bool, error, []map[string]interface{}) {
 	var data []map[string]interface{}
 	ref := client.NewRef("antrian")
 	if err := ref.Get(ctx, &data); err != nil {
 		log.Fatalln("Error reading from database:", err)
-		return false,err,nil
+		return false, err, nil
 	}
 
-	return true,nil,data
+	return true, nil, data
 }
 
-func updateAntrian(idAntrian string) (bool,error){
+func updateAntrian(idAntrian string) (bool, error) {
 	ref := client.NewRef("antrian")
 	id := strings.Split(idAntrian, "-")
 	childRef := ref.Child(id[1])
 	antrian := Antrian{
-		Id: idAntrian,
+		Id:     idAntrian,
 		Status: true,
 	}
 	if err := childRef.Set(ctx, antrian); err != nil {
 		log.Fatal(err)
-		return false,err
+		return false, err
 	}
 
-	return true,nil
+	return true, nil
 }
 
-func deleteAntrian (idAntrian string) (bool,error){
+func deleteAntrian(idAntrian string) (bool, error) {
 
 	ref := client.NewRef("antrian")
 	id := strings.Split(idAntrian, "-")
 	childRef := ref.Child(id[1])
 	if err := childRef.Delete(ctx); err != nil {
 		log.Fatal(err)
-		return false,err
+		return false, err
 	}
 
-	return true,nil
+	return true, nil
 }
 
 type Antrian struct {
-	Id string `json:"id"`
-	Status bool `json:"status"`
+	Id     string `json:"id"`
+	Status bool   `json:"status"`
 }
